b878421ca9d65370f0799874ad7b99ac2d6a5919
hrist <Kasperabc123!>
2021-03-02 20:12:16 +0100

    Add @JsonProperty to the record's property so it can be deserialized properly

--- services/ordering/src/main/java/com/eshop/ordering/api/application/commands/CancelOrderCommand.java
+++ services/ordering/src/main/java/com/eshop/ordering/api/application/commands/CancelOrderCommand.java
@@ -1,6 +1,7 @@
 package com.eshop.ordering.api.application.commands;
 
 import an.awesome.pipelinr.Command;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record CancelOrderCommand(Long orderNumber) implements Command<Boolean> {
+public record CancelOrderCommand(@JsonProperty("orderNumber") Long orderNumber) implements Command<Boolean> {
 }
