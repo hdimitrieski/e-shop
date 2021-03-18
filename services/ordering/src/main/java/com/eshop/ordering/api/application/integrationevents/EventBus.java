3654943aeb7cbfcc5e46c08801b90f4516bfdbad
hrist <Kasperabc123!>
2021-03-18 22:58:21 +0100

    Fix formatting

--- services/ordering/src/main/java/com/eshop/ordering/api/application/integrationevents/EventBus.java
+++ services/ordering/src/main/java/com/eshop/ordering/api/application/integrationevents/EventBus.java
@@ -2,5 +2,5 @@ package com.eshop.ordering.api.application.integrationevents;
 
 // TODO HD move in shared project (BuildingBlocks)
 public interface EventBus {
-    void publish(IntegrationEvent event);
+  void publish(IntegrationEvent event);
 }
