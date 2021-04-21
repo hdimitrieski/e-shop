49826d0e6f1df75d15abcfa863e2a68c5341c17b
hrist <Kasperabc123!>
2021-04-22 00:17:53 +0200

    Log the user after successful authentication in basket service

--- services/basket/src/main/java/com/eshop/basket/config/SecurityConfig.java
+++ services/basket/src/main/java/com/eshop/basket/config/SecurityConfig.java
@@ -1,28 +1,45 @@
 package com.eshop.basket.config;
 
-import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  @Value("${app.security.jwt.user-name-attribute}")
+  private String userNameAttribute;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
     http
-        .mvcMatcher("/basket/**")
+        .antMatcher("/basket/**")
         .authorizeRequests()
-        .mvcMatchers(HttpMethod.GET, "/basket/*").hasAuthority("SCOPE_basket")
-        .mvcMatchers(HttpMethod.POST, "/basket/*").hasAuthority("SCOPE_basket")
-        .mvcMatchers(HttpMethod.PUT, "/basket/*").hasAuthority("SCOPE_basket")
-        .mvcMatchers(HttpMethod.DELETE, "/basket/*").hasAuthority("SCOPE_basket")
+        .antMatchers(HttpMethod.GET, "/basket/*").hasAuthority("SCOPE_basket")
+        .antMatchers(HttpMethod.POST, "/basket/*").hasAuthority("SCOPE_basket")
+        .antMatchers(HttpMethod.PUT, "/basket/*").hasAuthority("SCOPE_basket")
+        .antMatchers(HttpMethod.DELETE, "/basket/*").hasAuthority("SCOPE_basket")
         .and()
         .oauth2ResourceServer()
-        .jwt();
+        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(jwt -> {
+          var token = new JwtAuthenticationConverter().convert(jwt);
+          return new JwtAuthenticationToken(jwt,
+              token.getAuthorities(), jwt.getClaim(userNameAttribute));
+        }));
+  }
 
-    return http.build();
+  @EventListener
+  public void authenticationSuccess(AuthenticationSuccessEvent event) {
+    logger.info("User {} authenticated", event.getAuthentication().getName());
   }
 
 }
