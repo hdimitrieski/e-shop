4fc1e5ce8ca04a2c031eeb5a06b7753f572e40f9
e-nechkoska <e.nechkoska@gmail.com>
2021-05-05 21:51:53 +0200

    implement catalog page component wip

---
+++ client/src/app/catalog/pages/catalog-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'es-catalog-page',
+  templateUrl: './catalog-page.component.html'
+})
+export class CatalogPageComponent {
+
+}
