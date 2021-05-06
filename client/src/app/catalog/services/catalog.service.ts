aa4be5657c51ec3a4647fc07f7da392801a6f84f
e-nechkoska <e.nechkoska@gmail.com>
2021-05-06 22:37:09 +0200

    add types to methods

--- client/src/app/catalog/services/catalog.service.ts
+++ client/src/app/catalog/services/catalog.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CatalogBrand } from '../models/catalogBrand';
+import { CatalogType } from '../models/catalogType';
+import { Page } from '../models/page';
 
 @Injectable()
 export class CatalogService {
   constructor(private http: HttpClient) {}
 
   fetchCatalogItems() {
-    return this.http.get('/api/v1/catalog/items');
+    return this.http.get('/api/v1/catalog/items') as Observable<Page>;
   }
 
   fetchCatalogTypes() {
-    return this.http.get('/api/v1/catalog/catalogtypes');
+    return this.http.get('/api/v1/catalog/catalogtypes') as Observable<CatalogType[]>;
   }
 
   fetchCatalogBrands() {
-    return this.http.get('/api/v1/catalog/catalogbrands');
+    return this.http.get('/api/v1/catalog/catalogbrands') as Observable<CatalogBrand[]>;
   }
 }
