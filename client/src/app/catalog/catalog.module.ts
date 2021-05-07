041f35516fafe8861a17881105ae9e5a73cce3d9
e-nechkoska <e.nechkoska@gmail.com>
2021-05-07 22:26:28 +0200

    update module

--- client/src/app/catalog/catalog.module.ts
+++ client/src/app/catalog/catalog.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CatalogComponent } from './components/catalog.component';
+import { FilterComponent } from './components/filter.component';
+import { ItemComponent } from './components/item.component';
 import { CatalogPageComponent } from './pages/catalog-page.component';
 import { CatalogService } from './services/catalog.service';
 
 @NgModule({
   declarations: [
+    FilterComponent,
+    CatalogComponent,
+    ItemComponent,
     CatalogPageComponent
   ],
   providers: [
@@ -13,7 +20,11 @@ import { CatalogService } from './services/catalog.service';
   ],
   imports: [
     CommonModule, 
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
+  exports: [
+    CatalogPageComponent
+  ]
 })
 export class CatalogModule {}
