7df4091196f15bbc3bf7170c8a0b2e39e3efc7f3
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2022-01-09 18:13:38 +0100

    feat(graphql-client): create initial graphql-client project

---
+++ clients/react-graphql-client/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER_URL,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
